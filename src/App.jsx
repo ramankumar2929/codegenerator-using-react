dd388dfc63f7a5ace2203e299e45b0c59387f229
ramankumar2929 <ramankumar72929@gmail.com>
2026-05-31 13:50:41 +0530

    something

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useCallback, useState } from 'react'
+import { memo } from 'react'
+
+
+function App() {
+const [length,setlength] = useState(8)
+const [noallowed, setnoallowed] = useState(false)
+const [charallowed, setcharallowed] = useState(true)
+
+
+return(
+
+   
+  <>
+   <h1 className="text-4xl text-center text-white my-5">Password Generator</h1>
+
+
+
+
+
+
+
+  </>
+)
+
+}
+
+export default App
